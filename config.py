0ffeab58c69d70984d93b7f18669df9decff1415
msayed20089 <serves208@gmail.com>
2025-10-05 20:05:58 +0300

    Update config.py

--- config.py
+++ config.py
@@ -6,11 +6,9 @@ CHANNEL_ID = os.getenv('CHANNEL_ID', "@Kingelg0ld")
 QX_SIGNUP_URL = "https://broker-qx.pro/sign-up/?lid=1376472"
 
 # بيانات الدخول لـ QX Broker
-QX_CREDENTIALS = {
-    'email': os.getenv('QX_EMAIL', 'mohamedels928@gmail.com'),
-    'password': os.getenv('QX_PASSWORD', 'Mrvip@219'),
-    'login_url': 'https://qxbroker.com/ar/sign-in/'
-}
+QX_EMAIL = os.getenv('QX_EMAIL', 'mohamedels928@gmail.com')
+QX_PASSWORD = os.getenv('QX_PASSWORD', 'Mrvip@219')
+QX_LOGIN_URL = 'https://qxbroker.com/ar/sign-in/'
 
 # الأزواج المطلوبة
 TRADING_PAIRS = [
@@ -20,9 +18,7 @@ TRADING_PAIRS = [
 ]
 
 # إعدادات التداول
-TRADE_SETTINGS = {
-    'duration_seconds': 30,
-    'trade_interval_minutes': 3,
-    'session_start': '06:00',
-    'session_end': '20:00'
-}
+TRADE_DURATION = 30
+TRADE_INTERVAL = 3
+SESSION_START = '06:00'
+SESSION_END = '20:00'
